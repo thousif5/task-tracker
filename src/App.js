7b542cff624fb010d9b2fdd96fa21eaf9a5b3656
Mohammed Thousif Shaik <thousif4023@gmail.com>
2021-02-03 00:32:50 +0530

    created basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Appointment',
+      day: 'Feb 5th',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting',
+      day: 'Feb 6th',
+      reminder: false
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
